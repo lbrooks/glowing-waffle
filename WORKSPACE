e9faae814c455fc06a88bede7ad9b53cd27a60ad
Lawrence Brooks <84.lbrooks@gmail.com>
2022-03-13 11:01:05 -0400

    roll the dice

---
+++ WORKSPACE
@@ -0,0 +1,30 @@
+##### Protobuf Rules for Bazel
+##### See https://github.com/bazelbuild/rules_proto
+load("@bazel_tools//tools/build_defs/repo:http.bzl", "http_archive")
+
+http_archive(
+    name = "rules_proto",
+    strip_prefix = "rules_proto-0bc8db13f8dc8f5d435be07e276712db1ca11a91",
+    urls = [
+        "https://mirror.bazel.build/github.com/bazelbuild/rules_proto/archive/0bc8db13f8dc8f5d435be07e276712db1ca11a91.tar.gz",
+        "https://github.com/bazelbuild/rules_proto/archive/0bc8db13f8dc8f5d435be07e276712db1ca11a91.tar.gz",
+    ],
+)
+load("@rules_proto//proto:repositories.bzl", "rules_proto_dependencies", "rules_proto_toolchains")
+rules_proto_dependencies()
+rules_proto_toolchains()
+
+##### gRPC Rules for Bazel
+##### See https://github.com/grpc/grpc/blob/master/src/cpp/README.md#make
+http_archive(
+    name = "com_github_grpc_grpc",
+    urls = [
+        "https://github.com/grpc/grpc/archive/5d81683faacdb1793d7eaca56cb80102ba2d96e2.tar.gz",
+    ],
+    strip_prefix = "grpc-5d81683faacdb1793d7eaca56cb80102ba2d96e2",
+)
+load("@com_github_grpc_grpc//bazel:grpc_deps.bzl", "grpc_deps")
+grpc_deps()
+# Not mentioned in official docs... mentioned here https://github.com/grpc/grpc/issues/20511
+load("@com_github_grpc_grpc//bazel:grpc_extra_deps.bzl", "grpc_extra_deps")
+grpc_extra_deps()
---
+++ dice/dice_server.cc
@@ -0,0 +1,113 @@
+/*
+ *
+ * Copyright 2015 gRPC authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+#include <iostream>
+#include <ctime>
+#include <cstdlib>
+#include <memory>
+#include <string>
+
+#include <grpcpp/grpcpp.h>
+
+#ifdef BAZEL_BUILD
+#include "proto/dice.grpc.pb.h"
+#else
+#include "proto/dice.grpc.pb.h"
+#endif
+
+using grpc::Server;
+using grpc::ServerBuilder;
+using grpc::ServerContext;
+using grpc::Status;
+using dice::DiceRollRequest;
+using dice::DiceRollResponse;
+using dice::DiceRollService;
+
+// Logic and data behind the server's behavior.
+class DiceRollServiceImpl final : public DiceRollService::Service {
+  Status Roll(ServerContext* context, const DiceRollRequest* request,
+                  DiceRollResponse* reply) override {
+    long res = 0;
+
+    std::cout << "Roll: ";
+    for (int i = 0; i < request->roll_size(); i++) {
+      if (i != 0) {
+        std::cout << " + ";
+      }
+      std::cout << request->roll(i);
+    }
+    std::cout << std::endl;
+
+    for (auto d : request->roll()) {
+      std::size_t found = d.find("d");
+      if (found != std::string::npos) {
+        int quantity = std::stoi(d.substr(0, found));
+        int sides = std::stoi(d.substr(found+1));
+
+        std::cout << "d" << sides << ": [";
+
+        for (int r = 0; r < quantity; r++) {
+          auto roll = (rand() % sides) + 1;
+          res += roll;
+          reply->add_roll(roll);
+
+          if (r != 0) {
+            std::cout << ", ";
+          }
+          std::cout << roll;
+        }
+
+        std::cout << "]" << std::endl;
+      } else {
+        res += std::stoi(d);
+      }
+    }
+
+    reply->set_total(res);
+    return Status::OK;
+  }
+};
+
+void RunServer() {
+  std::string address = "0.0.0.0";
+  std::string port = "50051";
+  std::string server_address = address + ":" + port;
+  DiceRollServiceImpl service;
+
+  ServerBuilder builder;
+  // Listen on the given address without any authentication mechanism.
+  builder.AddListeningPort(server_address, grpc::InsecureServerCredentials());
+  // Register "service" as the instance through which we'll communicate with
+  // clients. In this case it corresponds to an *synchronous* service.
+  builder.RegisterService(&service);
+  // Finally assemble the server.
+  std::unique_ptr<Server> server(builder.BuildAndStart());
+  std::cout << "Server listening on " << server_address << std::endl;
+
+  // Wait for the server to shutdown. Note that some other thread must be
+  // responsible for shutting down the server for this call to ever return.
+  server->Wait();
+}
+
+int main(int argc, char** argv) {
+  std::srand(std::time(0));  // Initialize random number generator.
+
+  RunServer();
+
+  return 0;
+}
